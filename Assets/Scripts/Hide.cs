7925a09eb2150ccd1df74bcbf01b3fae8bd130fe
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2021-05-01 03:02:46 -0300

    hiding mechanic

---
+++ Assets/Scripts/Hide.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour
+{
+
+    PlayerManager _currentManager;
+    bool _ableToHide = false;
+    Transform _currentTarget = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentManager = this.GetComponent<PlayerManager>();
+       
+    }
+
+    private void FixedUpdate()
+    {
+        HideAction();
+    }
+
+    private void HideAction()
+    {
+        if (InputController.hidingControl)
+        {
+            if (_ableToHide)
+            {
+                ChangeCurrentState();       
+            }
+        }
+    }
+
+    private void ChangeCurrentState()
+    {
+        if (_currentManager.PlayerState == PlayerState.RUNNING)
+        {
+            _currentManager.PlayerState = PlayerState.HIDING;
+           
+            this.transform.position = _currentTarget.position;
+        }
+        else
+        {
+            _currentManager.PlayerState = PlayerState.RUNNING;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Place"))
+        {
+            _ableToHide = true;
+            _currentTarget = collision.gameObject.transform.Find("HidingSpot");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Place"))
+        {
+            _ableToHide = false;
+            
+        }
+    }
+}
