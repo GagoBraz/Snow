9d91c2187981ebefba24bd53d61a71b332bf7596
Gagobraz <braz.lucas2912@gmail.com>
2021-05-04 19:41:16 -0300

    Uma animação inacabada e tristeza

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,6 +23,9 @@ public class Movement : MonoBehaviour
     float _MIN_VELOCITY_Y = 1.0f;
     #endregion  
 
+    int lastDirection;
+    public string[] staticDirections = { "Player_Static_NW","Player_Static_NL","Player_Static_SW","Player_Static_SL"};
+
     private void Start()
     {
         _rbody = this.GetComponent<Rigidbody2D>();
@@ -33,6 +36,27 @@ public class Movement : MonoBehaviour
     {
         CalculateMovement();
 
+    }
+
+    private int DirectionToIndex(Vector2 )
+    {
+        Vector2 norDir = _direction.normalized;
+
+        float step = 360 / 4;
+
+        float angle = Vector2.SignedAngle(Vector2.up, norDir);
+
+        if(angle < 0)
+        {
+            angle += 360;
+
+        }
+
+        float subangle = angle/2;
+
+        float stepCount = subangle/step;
+
+        return Mathf.FloorToInt(stepCount);
     }
 
     public void CalculateMovement()
@@ -48,8 +72,11 @@ public class Movement : MonoBehaviour
 
             Vector2 dir = _rbody.velocity.normalized;
 
+            
             Vector2 clampedVelocity = clampVelocity(_rbody.velocity);
 
+
+            directionArray = staticDirections;
 
             clampedVelocity.x = Mathf.Sign(dir.x) != Mathf.Sign(xInput) ? _MIN_VELOCITY_X : clampedVelocity.x;
             clampedVelocity.y = Mathf.Sign(dir.y) != Mathf.Sign(yInput) ? _MIN_VELOCITY_Y : clampedVelocity.y;
@@ -61,7 +88,10 @@ public class Movement : MonoBehaviour
         else
         {
             _rbody.velocity = Vector2.zero;
+
+            lastDirection = DirectionToIndex(_dir);
         }
+
     }
 
     private Vector2 clampVelocity(Vector2 velocity)
